c029044abac006e0a908ca59b6447ecece7aac07
Yoel Fernandez <afernandezs22_1@unc.edu.pe>
2026-09-24 09:05:13 -0500

    Delete addons/eigr_construction_management directory

--- addons/eigr_construction_management/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from . import construction_project
-from . import construction_budget
-from . import construction_valuation
-from . import construction_requirement
-from . import construction_closure
--- addons/eigr_construction_management/tests/__init__.py
+++
@@ -1,5 +0,0 @@
-from . import test_construction_project
-from . import test_construction_budget
-from . import test_construction_valuation
-from . import test_construction_requirement
-from . import test_construction_closure
